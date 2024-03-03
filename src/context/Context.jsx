b8a0fae7af52624484864edd5d771bcc2bc687c0
Vijay Ram kp <135154698+Vijaybbbb@users.noreply.github.com>
2024-03-03 12:08:47 +0530

    context creation

---
+++ src/context/Context.jsx
@@ -0,0 +1,18 @@
+import { Children, createContext } from "react";
+import { Firebase } from "../Firebase/config";
+import React from 'react'
+
+//creating context for firbase
+export const FirebaseContext = createContext(null)
+
+
+const Context = ({children}) => {
+  return (
+
+    <FirebaseContext.Provider value={{Firebase}}>
+      {children}
+    </FirebaseContext.Provider>
+  )
+}
+
+export default Context
